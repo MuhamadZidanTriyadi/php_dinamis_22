9b1888f8332d1b50edad63a4d1ba9f9d3dbdaf9b
MuhamadZidanTriyadi <muhammadzidantriyadi@gmamil.com>
2021-11-19 08:07:56 +0700

    ini versi pertama saya

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+
+          <!-- ini adalah pembuka navbar -->
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+              <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Zidan</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Alamat</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Hobi</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Cita - Cita</a>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- ini adalah penutup navbar -->
+                <div class="container">
+  <div class="row">
+    <div class="col-4">
+    <div class="card mx-auto" style="width: 18rem;">
+  <img src="assets/img/zidan.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Zidan</p>
+  </div>
+</div>
+    </div>
+    <div class="col-8">
+      <h1>Selamat datang di website saya</h1>
+      <P> Perkenalkan saya Muhamad Zidan Triyadi biasa dipanggil Zidan dan saya berada di kelas 11 RPL dan hobi saya bermain game dan berolahraga. Olahraga yang biasa lakukan seperti main Futsal dan Badminton.
+    </div>
+    
+  </div>
+</div>  
+
+
+            </div>
+            <!-- akhir pembungkus -->
+
+                <!-- LINK javascript -->
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+                
+            </body>
+            </html>
